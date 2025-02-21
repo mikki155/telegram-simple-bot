5e3b47adfb28e3fd3a596c6ca82daf27f7e6eb98
Mikael <mi_hau94@hotmail.com>
2025-02-21 16:48:19 +0100

    Bitcoin notification bot for big price changes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import requests
+import time
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 from dotenv import load_dotenv
@@ -5,11 +7,27 @@ import os
 
 load_dotenv()
 
+async def pollCryptoPrices(update: Update):
+    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&price_change_percentage=1h"
+    while True:
+        response = requests.get(url).json()
+        time.sleep(60)
+        btc_change_1h = response[0]["price_change_percentage_1h_in_currency"]
+        if abs(btc_change_1h) > 5:
+            await update.message.reply_text("Bitcoin is on the move! "
+                                            "1h percentage change: " + str(round(btc_change_1h, 2)) +
+                                            " " +
+                                            "https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400")
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Hello world! This is a simple telegram bot.')
+    await update.message.reply_text('Starting to poll cryptocurrency data...')
+    await pollCryptoPrices(update)
+
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('This is a simple hello world bot. More features will be added.')
+    await update.message.reply_text('This is a simple bot that polls price data every 15 minutes,'
+                                    'and shoots a notification if there is a big move.')
 
 def main():
 
