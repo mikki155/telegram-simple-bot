f89e0324063fec6437bed8cb6a58588577537675
Mikael <mi_hau94@hotmail.com>
2025-02-21 15:25:36 +0100

    Additional features

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+from dotenv import load_dotenv
+import os
 
-BOT_TOKEN = "7655658834:AAFSWlyiAbCjIOArNIErSWbCxhH_RNpxMJY"
+load_dotenv()
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Hello world! This is a simple telegram bot.')
 
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('This is a simple hello world bot. More features will be added.')
 
 def main():
-    application = Application.builder().token(BOT_TOKEN).build()
+
+    application = Application.builder().token(os.getenv('BOT_TOKEN')).build()
 
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help))
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
