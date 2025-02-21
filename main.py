75af9730f91c9e3cadc03f5f53d6b0e15df5047c
Mikael <mi_hau94@hotmail.com>
2025-02-21 15:13:35 +0100

    Simple Telegram bot

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello world!')
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+BOT_TOKEN = "7655658834:AAFSWlyiAbCjIOArNIErSWbCxhH_RNpxMJY"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('Hello world! This is a simple telegram bot.')
+
+
+def main():
+    application = Application.builder().token(BOT_TOKEN).build()
+
+    application.add_handler(CommandHandler("start", start))
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == '__main__':
+    main()
